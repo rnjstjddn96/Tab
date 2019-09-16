b8e00e0810ea4f652cbb482c6d43dfc59a52ef0a
rnjstjddn96 <rnjstjddn1996@gmail.com>
2019-09-16 18:56:59 +0900

    Tab/Nav Item Prac

--- Tab/ViewController.swift
+++ Tab/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnMoveImageView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 1
+    }
+    
+    
+    @IBAction func btnMoveDatePickerView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 2
+    }
 }
 
